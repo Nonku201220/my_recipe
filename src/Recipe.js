816d1d66cc049348b51fc685f6c583eb02476d3e
Nonkululeko <www.nonkululekontotoshe87@gmail.com>
2023-06-29 16:10:08 +0200

    added button

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,51 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { SwiperSlide } from 'swiper/react';
 import './App.css';
 
-const Recipe = ({ title, /* calories,  */image, ingredients }) => {
+const Recipe = ({ title, image, ingredients }) => {
+  const [showIngredients, setShowIngredients] = useState(false);
+
+  const toggleIngredients = () => {
+    setShowIngredients(!showIngredients);
+  };
+
   return (
-    <SwiperSlide>
-      <div style={styles.recipe}>
-        <h1>{title}</h1>
-        <img style={styles.image} src={image} alt='' />
-        <ol style={styles.ingredientsList}>
-          {ingredients && ingredients.map((ingredient, index) => (
+    <div className="recipe">
+      <h2>{title}</h2>
+      <img src={image} alt={title} />
+      <button onClick={toggleIngredients}>
+        {showIngredients ? 'Hide Ingredients' : 'Show Ingredients'}
+      </button>
+      {showIngredients && (
+        <ul>
+          {ingredients.map((ingredient, index) => (
             <li key={index}>{ingredient.text}</li>
           ))}
-        </ol>
-       {/*  <p>{calories}</p> */}
-      
-      </div>
-    </SwiperSlide>
+        </ul>
+      )}
+    </div>
   );
 };
 
 const styles = {
-    recipe: {
-      borderRadius: '10px',
-      boxShadow: '0px 5px 20px rgba(71, 71, 71)',
-      margin: '-100px',
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'space-around',
-      background: 'white',
-      marginLeft: '10rem', // Updated marginLeft value for wider background
-      alignItems: 'center',
-      minWidth: '40px',
-      marginTop: '5rem',
-    },
-    ingredientsList: {
-      listStyleType: 'decimal',
-      paddingLeft: '20px',
-    },
-    image: {
-      borderRadius: '50%',
-      width: '100px',
-      height: '100px',
-    },
-  };
-  
-  export default Recipe;
+  recipe: {
+    borderRadius: '10px',
+    boxShadow: '0px 5px 20px rgba(71, 71, 71)',
+    margin: '-100px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+    background: 'white',
+    marginLeft: '10rem', // Updated marginLeft value for wider background
+    alignItems: 'center',
+    minWidth: '40px',
+    marginTop: '5rem',
+  },
+  ingredientsList: {
+    listStyleType: 'decimal',
+    paddingLeft: '20px',
+  },
+  image: {
+    borderRadius: '50%',
+    width: '100px',
+    height: '100px',
+  },
+};
+
+export default Recipe;
 
 
 
