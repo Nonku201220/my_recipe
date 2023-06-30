cf728bedc6378f8fac31bb0e4420e70204b984de
Nonkululeko <www.nonkululekontotoshe87@gmail.com>
2023-06-30 11:10:19 +0200

    added button

--- src/App.js
+++ src/App.js
@@ -37,27 +37,29 @@ const App = () => {
 
   return (
     <div className="App">
-      <form onSubmit={getSearch} className="search-Form">
-        <input
-          className="search-bar"
-          type="text"
-          value={search}
-          onChange={updateSearch}
-        />
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
-      <div className="recipe-grid">
-        {recipes.map((recipe) => (
-          <div key={recipe.recipe.label}>
-            <Recipe
-              title={recipe.recipe.label}
-              image={recipe.recipe.image}
-              ingredients={recipe.recipe.ingredients || []}
-            />
-          </div>
-        ))}
+      <div className='Header'> Search Recipes
+        <form onSubmit={getSearch} className="search-Form">
+          <input
+            className="search-bar"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+        <div className="recipe-grid">
+          {recipes.map((recipe) => (
+            <div key={recipe.recipe.label}>
+              <Recipe
+                title={recipe.recipe.label}
+                image={recipe.recipe.image}
+                ingredients={recipe.recipe.ingredients || []}
+              />
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
